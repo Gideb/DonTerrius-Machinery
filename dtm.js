9a9fa9a281f9732e55c7525a51bf579bfbcfefb0
gideb <gideblarbi@gmail.com>
2025-07-26 13:25:51 +0000

    new edits

--- dtm.js
+++ dtm.js
@@ -1,30 +1,3 @@
-/* 
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-  import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
-
-  // Paste your Firebase config here
-  const firebaseConfig = {
-    apiKey: "AIza...your-key",
-    authDomain: "your-project.firebaseapp.com",
-    projectId: "your-project-id",
-    storageBucket: "your-project.appspot.com",
-    messagingSenderId: "123456789",
-    appId: "1:1234567890:web:abcdefg"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app);
-
-  // Save db to global window (optional but useful)
-  window.db = db;
-  window.addDoc = addDoc;
-  window.collection = collection;
-
-
- */
-
-
 fetch("header.html")
   .then((response) => response.text())
   .then((data) => {
@@ -33,9 +6,9 @@ fetch("header.html")
     const toggleMenuEl = document.getElementById("toggleMenu");
     const mainNav = document.getElementById("mainNav");
 
-    toggleMenuEl.addEventListener('click', () => {
-      mainNav.classList.toggle('active');
-    })
+    toggleMenuEl.addEventListener("click", () => {
+      mainNav.classList.toggle("active");
+    });
   });
 
 fetch("footer.html")
